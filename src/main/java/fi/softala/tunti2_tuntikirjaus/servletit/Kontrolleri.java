61ea739b47a516fb27cc92b6e03f5d98124ecd05
Ilvonen Tommi <a1500862@myy.haaga-helia.fi>
2016-09-15 12:05:45 +0300

    1. Kontrolleri.java - Muokattu LisaaTunnitServletistä

---
+++ src/main/java/fi/softala/tunti2_tuntikirjaus/servletit/Kontrolleri.java
@@ -0,0 +1,80 @@
+package fi.softala.tunti2_tuntikirjaus.servletit;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.omg.CORBA.Request;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import fi.softala.tunti2_tuntikirjaus.dao.TunnitDAO;
+import fi.softala.tunti2_tuntikirjaus.luokat.Kayttaja;
+import fi.softala.tunti2_tuntikirjaus.luokat.Tunnit;
+
+/**
+ * 
+ * @author Tommi Ilvonen
+ * 
+ *
+ */
+
+public class Kontrolleri extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+  
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ArrayList<Tunnit> tunnit = new ArrayList();
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring-config.xml");
+		TunnitDAO dao = (TunnitDAO) context.getBean("daoLuokka");
+		String Etunimi = request.getParameter("etunimi");
+		String Sukunimi = request.getParameter("sukunimi");
+		String strTunnit = request.getParameter("tunnit");
+		int Tunnit = new Integer(strTunnit);
+		String Kuvaus = request.getParameter("kuvaus");
+		
+		Tunnit uusitunti = new Tunnit();
+		Kayttaja kayttaja = new Kayttaja();
+		
+
+		uusitunti.setKuvaus(Kuvaus);
+		uusitunti.setTuntien_maara(Tunnit);
+		
+		if(Etunimi.equalsIgnoreCase("Niko")) {
+			kayttaja.setId(1);
+			kayttaja.addTunnit(uusitunti);
+		} else if (Etunimi.equalsIgnoreCase("Janne")) {
+			kayttaja.setId(2);
+			kayttaja.addTunnit(uusitunti);
+		} else if (Etunimi.equalsIgnoreCase("Tommi")) {
+			kayttaja.setId(3);
+			kayttaja.addTunnit(uusitunti);
+		} else if (Etunimi.equalsIgnoreCase("Daniel")) {
+			kayttaja.setId(4);
+			kayttaja.addTunnit(uusitunti);
+		} else if (Etunimi.equalsIgnoreCase("Mira")) {
+			kayttaja.setId(5);
+			kayttaja.addTunnit(uusitunti);
+		}
+		
+		dao.tallenna(uusitunti);
+		doGet(request, response);
+		
+		
+	}
+
+}
