48144ec1b730eb7b4a9aef431e4f0e3ae7c1184e
Jäppinen Janne <a1500864@myy.haaga-helia.fi>
2016-10-06 11:57:05 +0300

    1. TunnitImpl.java 2. messages_en/fi - kuvauksen validointi ja virheviestit.

--- src/main/java/fi/softala/tunti2_tuntikirjaus/luokat/TunnitImpl.java
+++ src/main/java/fi/softala/tunti2_tuntikirjaus/luokat/TunnitImpl.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Range;
@@ -26,6 +26,7 @@ public class TunnitImpl implements Tunnit {
 	
 	private String paivamaara;
 	
+	@Pattern(regexp = "[a-zA-ZäöåÄÖÅ0-9/./,/:/@/?/!/+/=/(/)/#/%/&/*//\"\'\t\n\r -]+")
 	@Size(min = 1, max = 1000)
 	private String kuvaus;
 	
